67c2068cf8a8d6f68782b616eccc7eac2f58d202
EhsanTgv <EhsanTgv@gmail.com>
2024-02-21 09:05:26 +0300

    Setup the project

--- app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
+++ app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
@@ -2,10 +2,15 @@ package com.taghavi.dualscreenwindowmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.window.layout.WindowInfoTracker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var windowInfoTracker: WindowInfoTracker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
     }
 }
