6ae7b9dc54e8e16140b44dead1d10d6d91fb73d2
EhsanTgv <EhsanTgv@gmail.com>
2024-03-28 09:33:17 +0300

    Visualize the WindowMetrics information

--- app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
+++ app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.taghavi.dualscreenwindowmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowMetricsCalculator
 import com.taghavi.dualscreenwindowmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
+
+        obtainWindowMetrics()
+    }
+
+    private fun obtainWindowMetrics() {
+        val wmc = WindowMetricsCalculator.getOrCreate()
+        val currentWM = wmc.computeCurrentWindowMetrics(this).bounds.flattenToString()
+        val maximumWM = wmc.computeMaximumWindowMetrics(this).bounds.flattenToString()
+        binding.windowMetrics.text =
+            "CurrentWindowMetrics: ${currentWM}\nMaximumWindowMetrics: $maximumWM"
     }
 }
