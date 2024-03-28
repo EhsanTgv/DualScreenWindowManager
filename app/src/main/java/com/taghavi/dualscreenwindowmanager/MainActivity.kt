495a97186c0ba4046c4be97a3b445fbb1c0cb058
EhsanTgv <EhsanTgv@gmail.com>
2024-03-28 09:21:48 +0300

    Setup the application UI

--- app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
+++ app/src/main/java/com/taghavi/dualscreenwindowmanager/MainActivity.kt
@@ -3,13 +3,16 @@ package com.taghavi.dualscreenwindowmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.window.layout.WindowInfoTracker
+import com.taghavi.dualscreenwindowmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var windowInfoTracker: WindowInfoTracker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
     }
